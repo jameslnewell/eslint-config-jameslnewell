c79919a36392d693561650e7a122056d8fe3fab1
James Newell <j.newell@nib.com.au>
2015-09-23 12:02:16 +1000

    fix path

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fs = require('fs');
+var path = require('path');
 var strip = require('strip-json-comments');
-module.exports = JSON.parse(strip(fs.readFileSync('./defaults.json').toString()));
+module.exports = JSON.parse(strip(fs.readFileSync(path.join(__dirname, 'defaults.json')).toString()));
