7faa5a6b2e7e5262ef15812ddf1694e3c173d71c
James Newell <j.newell@nib.com.au>
2015-10-13 16:21:24 +1100

    remove comments so I can load the non-default configs

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-var fs = require('fs');
-var path = require('path');
-var strip = require('strip-json-comments');
-module.exports = JSON.parse(strip(fs.readFileSync(path.join(__dirname, 'defaults.json')).toString()));
+module.exports = require('./defaults.json');
