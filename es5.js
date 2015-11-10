64bfb4c8ee3c9f7a11daa470aced62db2a5594b2
James Newell <j.newell@nib.com.au>
2015-11-11 08:53:12 +1100

    parens again

--- es5.js
+++ es5.js
@@ -18,7 +18,7 @@ module.exports = {
     'no-empty': 2,
     'no-ex-assign': 2,
     'no-extra-boolean-cast': 2,
-    'no-extra-parens': 0,
+    'no-extra-parens': [2, 'functions'],
     'no-extra-semi': 2,
     'no-func-assign': 2,
     'no-inner-declarations': 2,
